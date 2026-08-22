d7c1033dbdde966f26e3ec93c76eccd448a0d1b5
Alexander Magdaleno <alexrmt@stanford.edu>
2026-08-22 04:55:03 -0700

    ledc ccu config

---
+++ ledc.h
@@ -0,0 +1,16 @@
+//
+// Created by alexa on 8/22/2026.
+//
+
+#ifndef NEOPIXEL_LEDC_H
+#define NEOPIXEL_LEDC_H
+
+#define LEDC_CLK_REG 0x02001BF0
+#define LEDC_BUS_GATING_RST_REG 0x02001BFC
+
+
+
+
+
+
+#endif //NEOPIXEL_LEDC_H
